d097759727454417925461bfa7fd068a6b260f0f
always-creative <alwayscreative811@gmail.com>
2025-07-30 23:41:32 +0530

    Changing Attribute & Style Dynamically

---
+++ Basic/JavaScript/Dom Manipulation/02. Changing Attribute & Style Dynamically/script.js
@@ -0,0 +1,4 @@
+document.querySelector("#fname").value = "mark";
+let bannerDiv = document.querySelector(".home-page-banner");
+
+bannerDiv.style.backgroundColor = "#fff" // here this property is changeing background color
